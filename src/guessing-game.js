0b8362de1f30f28be1497de9b8d35246546b0694
A1vik <A11vik@ya.ru>
2019-09-25 14:21:57 +0400

    feat: complete task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,27 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.min = 0;
+        this.max = 0;
+        this.mid = 0;
+    }
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        const mid = Math.round((this.max + this.min) / 2);
+        // console.log('mid', mid);
+        return this.mid = mid;
     }
 
     lower() {
-
+        this.max = this.mid;
     }
 
     greater() {
-
+        this.min = this.mid;
     }
 }
 
